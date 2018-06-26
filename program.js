0645634d97a10590cfba3f55e7aed537b520c6d1
Pawel Bus <pawelbus.dev@outlook.com>
2018-06-26 13:43:41 +0200

    Add program.js

---
+++ program.js
@@ -0,0 +1,38 @@
+process.stdin.setEncoding('utf-8');
+
+function commands() {
+  process.stdout.write(
+    '\n-----------------------------\n' +
+    'Commands:\n' +
+    '- ver [show node version]\n' +
+    '- lang [show system language]\n' +
+    '- /exit [exit programm]\n' +
+    '-----------------------------\n\n');
+};
+
+commands();
+
+process.stdin.on('readable', function() {
+  var input = process.stdin.read();
+
+  if (input !== null) {
+    var instruction = input.toString().trim();
+
+    switch (instruction) {
+      case 'commands':
+        commands();
+        break;
+      case 'ver':
+        console.log('Node version: ' + process.versions.node + '\n');
+        break;
+      case 'lang':
+        console.log('System language: ' + process.env.lang + '\n');
+        break;
+      case '/exit':
+        console.log('Quitting app!');
+        process.exit();
+      default:
+        console.log('Wrong instruction!' + '\n');
+    }
+  }
+});
